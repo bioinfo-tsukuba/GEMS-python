1aeb6825f6d871a869286037b2abe71aa20fbc78
CAB314 <arai.yuya.qa@alumni.tsukuba.ac.jp>
2024-08-01 14:14:43 +0900

    We can use simulated annealing schedule method (but simple one)

--- gems_python/transition_manager.py
+++ gems_python/transition_manager.py
@@ -7,9 +7,6 @@ import uuid
 import polars as pl
 from pathlib import Path
 
-
-
-
 """MODULE: Penalty
 """
 
@@ -181,11 +178,221 @@ class CyclicalRestPenaltyWithLinear(PenaltyType):
             if start <= diff <= end:
                 return PENALTY_MAXIMUM
         # Thank you for your hard work.
-        return abs(diff * self.penalty_coefficient)
+        return abs(scheduled_timing - optimal_timing) * self.penalty_coefficient
 
+
+OneMachineTaskLocalInformation = Tuple[str, int, Type[PenaltyType]]
+
+from simanneal import Annealer
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import random
+@dataclass
+class OneMachineTask:
+    """
+    OneMachineTask class.
+    This class is used as a data class for the task.
+    """
+    optimal_timing: int
+    processing_time: int
+    penalty_type: Type[PenaltyType]
+    experiment_operation: str
+    experiment_name: str
+    experiment_uuid: str
+    task_id: int
+    scheduled_timing: int = field(default=None)
+
+    def to_dict(self) -> dict:
+        """
+        Converts the task object to a dictionary.
+        :return: Dictionary representation of the task object.
+        """
+        return asdict(self)
+
+    @classmethod
+    def from_dict(cls, data: dict) -> 'OneMachineTask':
+        """
+        Creates a task object from a dictionary.
+        :param data: Dictionary containing the task data.
+        :return: Task object.
+        """
+        return cls(**data)
+
+    def to_json(self) -> str:
+        """
+        Converts the task object to a JSON string.
+        :return: JSON string representation of the task object.
+        """
+        data = self.to_dict()
+        return json.dumps(data)
+
+    @classmethod
+    def from_json(cls, json_str: str) -> 'OneMachineTask':
+        """
+        Creates a task object from a JSON string.
+        :param json_str: JSON string containing the task data.
+        :return: Task object.
+        """
+        data = json.loads(json_str)
+        return cls.from_dict(data)
+
+    @classmethod
+    def simulated_annealing_schedule(cls, tasks: List['OneMachineTask']) -> List['OneMachineTask']:
+        """
+        Simulated annealing scheduler.
+        :param tasks: List of tasks to schedule.
+        :return: Scheduled tasks.
+        """
+        class TaskAnnealer(Annealer):
+
+            def __init__(self, state):
+                super().__init__(state)
+                self.step_count: int = 0
+
+            def move(self):
+                self.step_count += 1
+
+                """
+                random move or swap
+                """
+                temp = max(1, int(self.step_count/self.steps * (self.Tmax - self.Tmin) + self.Tmin))
+                for i in range(min(temp, len(self.state))):
+                  a = random.randint(0, len(self.state) - 1)
+                  self.state[a].scheduled_timing += random.randint(-int(temp), int(temp))
+                  self.state[a].scheduled_timing = max(0, self.state[a].scheduled_timing)
+
+                if random.random() < 0.5:
+                    a = random.randint(0, len(self.state) - 1)
+                    b = random.randint(0, len(self.state) - 1)
+                    self.state[a].scheduled_timing, self.state[b].scheduled_timing = self.state[b].scheduled_timing, self.state[a].scheduled_timing
+
+            def energy(self):
+                """Calculates the total penalty for the current state."""
+                total_penalty = 0
+                for task in self.state:
+                    total_penalty += task.penalty_type.calculate_penalty(
+                        task.scheduled_timing, task.optimal_timing
+                    )
+
+                # Overlapping penalty
+                sorted_tasks = sorted(self.state, key=lambda x: x.scheduled_timing)
+                for i in range(len(sorted_tasks) - 1):
+                    task1 = sorted_tasks[i]
+                    task2 = sorted_tasks[i + 1]
+                    overlap = task1.scheduled_timing + task1.processing_time - task2.scheduled_timing
+                    if overlap > 0:
+                        total_penalty += overlap * 100000
+                return total_penalty
+
+        # Initialize the tasks with some initial schedule (e.g., their optimal timings)
+        time = 0
+        tasks = sorted(tasks, key=lambda x: x.optimal_timing + x.processing_time)
+        for task in tasks:
+            task.scheduled_timing = max(time, task.optimal_timing)
+            time = task.scheduled_timing + task.processing_time
+
+        # Create an instance of the annealer with the initial state
+        annealer = TaskAnnealer(tasks)
+        # Set the annealing parameters as needed
+        annealer.steps = 100000
+        annealer.Tmax = 25000.0
+        annealer.Tmin = 1.0
+
+        # Run the annealing process
+        state, _ = annealer.anneal()
+
+        return state
+
+
+    @classmethod
+    def vis(cls, tasks: List['OneMachineTask']):
+        """
+        Visualizes the scheduled tasks as a Gantt chart.
+        :param tasks: List of scheduled tasks to visualize.
+        """
+        fig, ax = plt.subplots()
+        tasks = sorted(tasks, key=lambda x: x.scheduled_timing)
+
+        # ガントチャートのデータを準備
+        for task in tasks:
+            start_time = task.scheduled_timing
+            end_time = start_time + task.processing_time
+            ax.barh(task.experiment_operation, end_time - start_time, left=start_time, edgecolor='black')
+
+        # 軸のラベルを設定
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Task')
+
+        # time軸の最大値と最小値を設定
+        max_time = max(task.scheduled_timing + task.processing_time for task in tasks)
+        max_time = max(max(task.optimal_timing + task.processing_time for task in tasks), max_time) + 5
+        min_time = min(task.scheduled_timing for task in tasks)
+        min_time = min(min(task.optimal_timing for task in tasks), min_time) - 5
+        ax.set_xlim(min_time, max_time)
+
+        # タスクごとに異なる色を設定
+        colors = plt.cm.get_cmap('tab10', len(tasks))
+        for i, task in enumerate(tasks):
+            start_time = task.scheduled_timing
+            end_time = start_time + task.processing_time
+            ax.barh(task.experiment_operation, end_time - start_time, left=start_time, color=colors(i), edgecolor='black')
+
+        # 凡例を設定
+        patches = [mpatches.Patch(color=colors(i), label=task.experiment_operation) for i, task in enumerate(tasks)]
+        plt.legend(handles=patches)
+
+        # グラフを表示
+        plt.show()
+
+
+    @classmethod
+    def vis_with_diff(cls, tasks: List['OneMachineTask']):
+        """
+        Visualizes the scheduled tasks as a Gantt chart with differences between optimal and scheduled timings.
+        :param tasks: List of scheduled tasks to visualize.
+        """
+        fig, ax = plt.subplots()
+        tasks = sorted(tasks, key=lambda x: x.scheduled_timing)
+
+        # ガントチャートのデータを準備
+        for task in tasks:
+            start_time = task.scheduled_timing
+            end_time = start_time + task.processing_time
+            optimal_start = task.optimal_timing
+            ax.barh(task.experiment_operation, end_time - start_time, left=start_time, edgecolor='black', alpha=0.7)
+
+            # optimal_timingとscheduled_timingの差を表す線を追加
+            if start_time != optimal_start:
+                ax.plot([optimal_start, start_time], [task.experiment_operation, task.experiment_operation], 'r--', linewidth=2)
+
+        # 軸のラベルを設定
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Task')
+
+        # time軸の最大値と最小値を設定
+        max_time = max(task.scheduled_timing + task.processing_time for task in tasks)
+        max_time = max(max(task.optimal_timing + task.processing_time for task in tasks), max_time) + 5
+        min_time = min(task.scheduled_timing for task in tasks)
+        min_time = min(min(task.optimal_timing for task in tasks), min_time) - 5
+        ax.set_xlim(min_time, max_time)
+
+        # タスクごとに異なる色を設定
+        colors = plt.cm.get_cmap('tab10', len(tasks))
+        for i, task in enumerate(tasks):
+            start_time = task.scheduled_timing
+            end_time = start_time + task.processing_time
+            ax.barh(task.experiment_operation, end_time - start_time, left=start_time, color=colors(i), edgecolor='black')
+
+        # 凡例を設定
+        patches = [mpatches.Patch(color=colors(i), label=task.experiment_operation) for i, task in enumerate(tasks)]
+        plt.legend(handles=patches)
+
+        # グラフを表示
+        plt.show()
 
 """MODULE: State
 """
+
 
 @dataclass
 class State(ABC):
@@ -209,7 +416,7 @@ class State(ABC):
         pass
 
     @abstractmethod
-    def task_generator(self, df: pl.DataFrame) -> Tuple[str, int, Type[PenaltyType]]:
+    def task_generator(self, df: pl.DataFrame) -> OneMachineTaskLocalInformation:
         pass
 
 """MODULE: Experiment
@@ -238,7 +445,7 @@ class Experiment:
     shared_variable_history: pl.DataFrame  # mutability is required
     parent_dir_path: Path # The parent directory path of the experiment
     experiment_uuid: str = field(default_factory=lambda: str(uuid.uuid4()))
-    current_task: Tuple[str, int, Type[PenaltyType]] = field(default=None)
+    current_task: OneMachineTaskLocalInformation = field(default=None)
 
     def __post_init__(self):
         """
@@ -264,7 +471,7 @@ class Experiment:
     
 
 
-    def execute_one_step(self) -> Tuple[str, int, type[PenaltyType]]:
+    def execute_one_step(self) -> OneMachineTaskLocalInformation:
         """
         Execute one step. Determine the next state index and generate a task.
         Note: The shared variable history is updated.
@@ -290,7 +497,7 @@ class Experiment:
         return task
     
 
-    def generate_task_of_the_state(self) -> Tuple[str, int, Type[PenaltyType]]:
+    def generate_task_of_the_state(self) -> OneMachineTaskLocalInformation:
         """
         Generate a task of the state.
         :return: Generated task.
--- tests/test_transition_manager.py
+++ tests/test_transition_manager.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from typing import Any, Tuple, Type
 import unittest
 import polars as pl
-from gems_python.transition_manager import NonePenalty, PenaltyType, State
+from gems_python.transition_manager import LinearPenalty, NonePenalty, OneMachineTask, PenaltyType, State
 
 @dataclass
 class MyState(State):
@@ -43,6 +43,29 @@ class TestTransitionManager(unittest.TestCase):
         self.assertEqual(task_name, "example_task")
         self.assertEqual(task_count, 3)
         print(penalty)
+
+
+
+class TestScheduler(unittest.TestCase):
+    def test_simulated_annealing_schedule(self):
+        tasks = [
+            OneMachineTask(optimal_timing=1, processing_time=5, penalty_type=LinearPenalty(penalty_coefficient=10), experiment_operation="op1", experiment_name="exp1", experiment_uuid="uuid1", task_id=1),
+            OneMachineTask(optimal_timing=3, processing_time=3, penalty_type=LinearPenalty(penalty_coefficient=5), experiment_operation="op2", experiment_name="exp2", experiment_uuid="uuid2", task_id=2),
+            OneMachineTask(optimal_timing=5, processing_time=8, penalty_type=LinearPenalty(penalty_coefficient=8), experiment_operation="op3", experiment_name="exp3", experiment_uuid="uuid3", task_id=3),
+            OneMachineTask(optimal_timing=5, processing_time=8, penalty_type=LinearPenalty(penalty_coefficient=8), experiment_operation="op4", experiment_name="exp3", experiment_uuid="uuid4", task_id=4),
+            OneMachineTask(optimal_timing=90, processing_time=8, penalty_type=LinearPenalty(penalty_coefficient=8), experiment_operation="op5", experiment_name="exp3", experiment_uuid="uuid5", task_id=5),
+            OneMachineTask(optimal_timing=50, processing_time=8, penalty_type=LinearPenalty(penalty_coefficient=8), experiment_operation="op6", experiment_name="exp3", experiment_uuid="uuid6", task_id=6),
+            OneMachineTask(optimal_timing=30, processing_time=8, penalty_type=LinearPenalty(penalty_coefficient=8), experiment_operation="op7", experiment_name="exp3", experiment_uuid="uuid7", task_id=7),
+            OneMachineTask(optimal_timing=20, processing_time=8, penalty_type=LinearPenalty(penalty_coefficient=8), experiment_operation="op8", experiment_name="exp3", experiment_uuid="uuid8", task_id=8),
+            OneMachineTask(optimal_timing=60, processing_time=8, penalty_type=LinearPenalty(penalty_coefficient=8), experiment_operation="op9", experiment_name="exp3", experiment_uuid="uuid9", task_id=9),
+        ]
+
+        scheduled_tasks = OneMachineTask.simulated_annealing_schedule(tasks)
+        OneMachineTask.vis(scheduled_tasks)
+        OneMachineTask.vis_with_diff(scheduled_tasks)
+
+        for t in scheduled_tasks:
+            print(t)
 
 if __name__ == '__main__':
     unittest.main()
